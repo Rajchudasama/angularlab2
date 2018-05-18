125a2e902fa207b4eea2d2e2caff7bf6aeb60295
raj <rajchudasam@outlook.com>
2018-05-18 13:27:02 -0400

    Birthday greeting function added

--- lab2.js
+++ lab2.js
@@ -1,3 +1,4 @@
+//enums declaration
 var days;
 (function (days) {
     days[days["Sunday"] = 0] = "Sunday";
@@ -23,6 +24,44 @@ var months;
     months[months["November"] = 10] = "November";
     months[months["December"] = 11] = "December";
 })(months || (months = {}));
+//html elements
 var pTodayDate = document.getElementById("p--today-date");
+var btnclick = document.getElementById("button--birthday");
+var getFormDate = document.getElementById("input--date-picker");
+var DisplayBirthdate = document.getElementById("p--birthday-message");
+//anynomous function on click event
+btnclick.onclick = function () {
+    var ubirthday = getFormDate.value;
+    var userBday = new Date(ubirthday);
+    DisplayBirthdate.innerHTML = makeDateString(userBday);
+};
+// function to making date string
+function makeDateString(inputDate) {
+    if ((inputDate.getDate() === today.getDate()) && (inputDate.getMonth() === today.getMonth())) {
+        return "Happy BirthDay!";
+    }
+    var currentYearBirthday = new Date();
+    currentYearBirthday.setDate(inputDate.getDate());
+    currentYearBirthday.setMonth(inputDate.getMonth());
+    currentYearBirthday.setFullYear(today.getFullYear());
+    return "your birthday is " + days[currentYearBirthday.getDay()] + " " + months[currentYearBirthday.getMonth()] + " " + currentYearBirthday.getDate() + ", " + currentYearBirthday.getFullYear();
+}
+//current date
 var today = new Date();
+//display date using string template
 pTodayDate.innerHTML = "Today is " + days[today.getDay()] + " " + months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
+/*function getBirthdate():void {
+    //form inputs
+    let getFormDate =<HTMLInputElement>document.getElementById("input--date-picker");
+    let DisplayBirthdate = document.getElementById("p--birthday-message");
+    //current date
+    let currentdate = new Date();
+    //birth date
+    let birthDate:Date = new Date(getFormDate.value)
+    // constructing current year birth date
+    let currentBirthdate:Date = new Date(currentdate.getFullYear(),birthDate.getMonth(),birthDate.getDate());
+    //displaying birthdate
+    DisplayBirthdate.innerHTML = `Your BirthDate is ${days[currentBirthdate.getDay()]} ${months[currentBirthdate.getMonth()]} ${currentBirthdate.getDate()}, ${currentBirthdate.getFullYear()}`;
+    console.log(currentBirthdate);
+
+}*/ 
--- lab2.ts
+++ lab2.ts
@@ -1,3 +1,5 @@
+
+//enums declaration
 enum days {
     Sunday,
     Monday,
@@ -22,9 +24,57 @@ enum months {
     November,
     December
 }
-
+//html elements
 let pTodayDate = document.getElementById("p--today-date");
+let btnclick = document.getElementById("button--birthday");
+let getFormDate =<HTMLInputElement>document.getElementById("input--date-picker");
+let DisplayBirthdate = document.getElementById("p--birthday-message");
 
+//anynomous function on click event
+btnclick.onclick = function(){
+    let ubirthday:string = getFormDate.value;
+    let userBday:Date = new Date(ubirthday);
+    DisplayBirthdate.innerHTML = makeDateString(userBday);
+
+}
+
+
+ 
+
+
+// function to making date string
+function makeDateString(inputDate:Date):string{
+    if( (inputDate.getDate()===today.getDate()) && (inputDate.getMonth()===today.getMonth()) ){
+        return `Happy BirthDay!`;
+    }
+
+    let currentYearBirthday = new Date();
+    currentYearBirthday.setDate(inputDate.getDate());
+    currentYearBirthday.setMonth(inputDate.getMonth());
+    currentYearBirthday.setFullYear(today.getFullYear());
+    return `your birthday is ${days[currentYearBirthday.getDay()]} ${months[currentYearBirthday.getMonth()]} ${currentYearBirthday.getDate()}, ${currentYearBirthday.getFullYear()}`
+
+}
+
+//current date
 let today:Date = new Date();
-
+//display date using string template
 pTodayDate.innerHTML = `Today is ${days[today.getDay()]} ${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
+
+
+
+/*function getBirthdate():void {
+    //form inputs
+    let getFormDate =<HTMLInputElement>document.getElementById("input--date-picker");
+    let DisplayBirthdate = document.getElementById("p--birthday-message");
+    //current date
+    let currentdate = new Date();
+    //birth date
+    let birthDate:Date = new Date(getFormDate.value)
+    // constructing current year birth date
+    let currentBirthdate:Date = new Date(currentdate.getFullYear(),birthDate.getMonth(),birthDate.getDate());
+    //displaying birthdate
+    DisplayBirthdate.innerHTML = `Your BirthDate is ${days[currentBirthdate.getDay()]} ${months[currentBirthdate.getMonth()]} ${currentBirthdate.getDate()}, ${currentBirthdate.getFullYear()}`;
+    console.log(currentBirthdate);
+
+}*/
